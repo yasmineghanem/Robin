532ee9093e4f7e0e9213407e5bd22e2a714a622a
ahmedmadbouly186 <66012617+ahmedmadbouly186@users.noreply.github.com>
2024-06-20 00:03:27 +0300

    change the URL to use the main camera

--- Mouse Tracking/main.py
+++ Mouse Tracking/main.py
@@ -5,7 +5,7 @@ import threading
 HTTP = 'https://'
 IP_ADDRESS = '192.168.1.86'
 URL =  HTTP + IP_ADDRESS + ':4343/video'
-
+URL = 0
 # eye_controller_object = eye_controller(URL)    
 # frame_thread = threading.Thread(target=eye_controller_object.process_frames)
 # frame_thread.start()
