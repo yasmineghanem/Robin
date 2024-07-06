bb7662cd073cc42c6e6e119190d6321205561271
Sarah Elzayat <sarahelzayat@outlook.com>
2024-07-07 00:28:29 +0300

    robin activation command

--- Command Execution/dist/extension.js
+++ Command Execution/dist/extension.js
@@ -25383,16 +25383,44 @@ exports.RUN_PYTHON = "robin.runPython";
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+const vscode = __importStar(__webpack_require__(1));
 const fileSystemCommands_1 = __importDefault(__webpack_require__(171));
 const IDECommands_1 = __importDefault(__webpack_require__(172));
 const codeCommands_1 = __importDefault(__webpack_require__(173));
 const gitCommands_1 = __importDefault(__webpack_require__(179));
+const activateRobin = () => vscode.commands.registerCommand('robin.activate', () => {
+    vscode.window.showInformationMessage('Robin Activated!');
+});
 // register commands 
 const registerAllCommands = () => {
+    activateRobin();
     (0, fileSystemCommands_1.default)();
     (0, IDECommands_1.default)();
     (0, codeCommands_1.default)();
--- Command Execution/src/commands/commands.ts
+++ Command Execution/src/commands/commands.ts
@@ -1,10 +1,15 @@
+import * as vscode from 'vscode';
 import registerFileSystemCommands from './fileSystemCommands';
 import registerIDECommands from './IDECommands';
 import registerCodeCommands from './codeCommands';
 import registerGITCommands from './gitCommands';
 
+const activateRobin = () => vscode.commands.registerCommand('robin.activate', () => {
+  vscode.window.showInformationMessage('Robin Activated!');
+});
 // register commands 
 const registerAllCommands = () => {
+  activateRobin();
   registerFileSystemCommands();
   registerIDECommands();
   registerCodeCommands();
